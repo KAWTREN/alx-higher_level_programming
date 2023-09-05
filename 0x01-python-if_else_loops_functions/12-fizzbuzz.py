5eb28293af731a2e2c6a3fc5c0f48329cbfab3c7
kawtar <kawtarennabirha@gmail.com>
2023-09-05 11:03:08 -0700

    p

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-for i in range(1, 101):
-    j = i % 3
-    k = i % 5
-    if j == 0 and k == 0:
-        print("FizzBuzz", end=" ")
-    elif k == 0:
-        print("Buzz", end=" ")
-    elif j == 0:
-        print("Fizz", end=" ")
-    else:
-        print(i, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        j = i % 3
+        k = i % 5
+        if j == 0 and k == 0:
+            print("FizzBuzz", end=" ")
+        elif k == 0:
+            print("Buzz", end=" ")
+        elif j == 0:
+            print("Fizz", end=" ")
+        else:
+            print("{:d}".format(i), end=" ")
