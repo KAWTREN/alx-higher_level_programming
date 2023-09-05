885b31a44629e5093d0d5a79905d3a32e6060ddd
kawtar <kawtarennabirha@gmail.com>
2023-09-05 10:54:10 -0700

    p

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+for i in range(1, 101):
+    j = i % 3
+    k = i % 5
+    if j == 0 and k == 0:
+        print("FizzBuzz", end=" ")
+    elif k == 0:
+        print("Buzz", end=" ")
+    elif j == 0:
+        print("Fizz", end=" ")
+    else:
+        print(i, end=" ")
