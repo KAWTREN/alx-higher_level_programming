5dbf14d10b3787a0f9bdc355d903ed765354e62f
kawtar <kawtarennabirha@gmail.com>
2023-09-07 06:16:14 -0700

    p

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import sys
     n = len(sys.argv)
     print(n-1, "argument:")
-    for i in range(1 , n):
-        print(i,":", sys.argv[i])
+    for i in range(1, n):
+        print(i, ":", sys.argv[i])
