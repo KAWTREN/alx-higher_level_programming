e63674ed2ff09d74496487a00d3d8d66dd460fbe
kawtar <kawtarennabirha@gmail.com>
2023-09-13 10:17:53 -0700

    p

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
+    unique_set = set()
     r = 0
     for i in my_list:
-        r += i
+        if  i not in unique_set:
+            unique_set.add(i)
+            r += i
     return r
