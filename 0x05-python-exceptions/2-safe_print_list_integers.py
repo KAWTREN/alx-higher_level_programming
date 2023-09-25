03e9e9b0d7e3ef19dd85acd752a97122002a301b
kawtar <kawtarennabirha@gmail.com>
2023-09-25 04:04:50 -0700

    p

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    cint = 0
     try:
-        cint = 0
         for i in range(x):
             if type(my_list[i]) is int:
                 print("{:d}".format(my_list[i]), end="")
