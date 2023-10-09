ec0d3f214103df22c12d1c458c46dcaef474ec20
kawtar <kawtarennabirha@gmail.com>
2023-10-09 03:45:05 -0700

    p

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/pythoni3
+"""available attributes"""
+
+
 def lookup(obj):
     """ Returns the list of available attributes and methods of an object"""
     return dir(obj)
