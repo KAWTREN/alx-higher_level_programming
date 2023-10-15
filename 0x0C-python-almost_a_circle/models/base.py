df4fa0105003d924c7a62fdd51e3f5affdd16726
kawtar <kawtarennabirha@gmail.com>
2023-10-15 10:18:07 -0700

    p

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3\
+"""base class modul"""
+
+
+class Base:
+    """Base class"""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """int
+        args:
+        id : int
+        """
+        if id is not None:
+            self.id = id
+        else:
+            Bese.__nb_objects += 1
+            self.id = Bese.__nb_objects
---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""rectangle class models"""
+
+from models.Base import Base
+
+class rectangle:
+    """rectangle class"""
+
