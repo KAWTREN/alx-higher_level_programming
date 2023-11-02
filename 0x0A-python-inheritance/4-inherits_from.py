63f6b08657aeaa60cd2c09f9241aeb7dd4cd5fff
KAWTREN <133163611+KAWTREN@users.noreply.github.com>
2023-11-02 16:36:17 +0100

    Delete 0x0A-python-inheritance directory

--- 0x0A-python-inheritance/4-inherits_from.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""is sub class (inherits) """
-
-
-def inherits_from(obj, a_class):
-    """returns True if the object is an instance of, or if the object is an
-      instance of a class; otherwise False."""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
