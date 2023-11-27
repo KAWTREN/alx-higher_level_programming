886003a86354a6aa880d8827ec2d1727fcabb6cf
kawtar <kawtarennabirha@gmail.com>
2023-11-27 12:11:01 +0100

    p

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,20 +1,34 @@
 #!/usr/bin/python3
+"""1-Rectangle module"""
+
+
 class Rectangle:
+    """Rectangle class"""
+   
     def __init__(self, width=0, height=0):
         self.width = width
         self.height= height
+    @property
     def width(self):
+        """width getter method"""
         return self.__width
+    
+    @width.setter
     def width(self, value):
+        """width setter methed"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
             raise ValueError("width must be >= 0")
         else:
             self.__width = value
+    @property
     def height(self):
+        """height getter"""
         return self.__height
+    @height.setter
     def height(self, value):
+        """height setter method"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
