126bdb02b59f46e7d84c93dc56a905dc25baf951
kawtar <kawtarennabirha@gmail.com>
2023-12-04 20:08:47 +0100

    p

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""base_geometry"""
+
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """class rectangle"""
+    
+    def __init__(self, width, height):
+        """instantioation"""
+        
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", self.__width)
+        self.integer_validator("height", self.__height)
+    def area(self):
+        """calculate and returns the area"""
+        return self.__width * self.__height
+    def __str__(self):
+        """rectangle string"""
+        return f"[Rectangle] {self.__width} /{self.__height}"
+    
