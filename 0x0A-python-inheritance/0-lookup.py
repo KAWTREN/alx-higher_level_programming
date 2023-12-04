9487a41cc42f024fb3feedbe5c5f5ca0350d96a5
kawtar <kawtarennabirha@gmail.com>
2023-12-04 11:45:22 +0100

    p

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""lookup class"""
+
+
 def lookup(obj):
-    """
-    list of available attributes and methods of an object"""
+    """list of available attributes and methods of an object"""
+    
     return [dir(obj)]
