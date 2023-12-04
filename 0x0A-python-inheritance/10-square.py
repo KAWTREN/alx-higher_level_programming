d32a9c2c0ac5026ce457f7534de89859a602e3fb
kawtar <kawtarennabirha@gmail.com>
2023-12-04 20:28:19 +0100

    p

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""base_geometry"""
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+class Square(Rectangle):
+    """square class"""
+    def __init__(self, size):
+        """init"""
+        self.__size = size
+        self.integer_validator("size", self.__size)
+        Rectangle.__init__(self, self.__size, self.__size)    
+    def area (self):
+        """calculates area"""
+        return (self.__size) ** 2
