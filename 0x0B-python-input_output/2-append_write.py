ba6e559551486a421f892beb6cfc9cd9777853ad
kawtar <kawtarennabirha@gmail.com>
2023-12-05 11:42:51 +0100

    p

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""append_string"""
 def append_write(filename="", text=""):
+    """appends a string at the end of text file and returns the number of charachter"""
     with open(filename, 'a', encoding='utf-8') as file:
         return(file.write(text))
