3a8536c0d6cb79456d5f1591febe2049322aca77
kawtar <kawtarennabirha@gmail.com>
2023-12-05 11:56:13 +0100

    p

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+import json
+
+"""to_json_string"""
+
+def to_json_string(my_obj):
+    """returns the JSON representation of an object"""
+    return(json.dumps(my_obj))
