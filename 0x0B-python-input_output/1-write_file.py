5a4758e08a961e2b045de84619ac0592be011add
kawtar <kawtarennabirha@gmail.com>
2023-12-05 11:26:22 +0100

    p

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""write_string"""
+
 def write_file(filename="", text=""):
+    """write a string and return the number of charachters written"""
+    
     with open(filename, 'w', encoding= 'utf-8') as file:
         return(file.write(text))
