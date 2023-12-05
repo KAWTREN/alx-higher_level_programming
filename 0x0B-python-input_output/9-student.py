4e60ddef2f5c0df80dff04d7c529520b310130ae
kawtar <kawtarennabirha@gmail.com>
2023-12-05 18:23:03 +0100

    p

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """student to json"""
 
-class_to_json = __import__('8-class_to_json').class_to_json
 
 class Student:
     """Student class"""
@@ -11,4 +10,4 @@ class Student:
         self.age = age
     def to_json(self):
         """retrieves a dictionary representation of a Student"""
-        return class_to_json(self)
+        return self.__dict__
