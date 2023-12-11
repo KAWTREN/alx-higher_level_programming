03df17b7d7bf258f2784b66b6611dad39bfaa88d
kawtar <kawtarennabirha@gmail.com>
2023-12-11 21:06:28 +0100

    p

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,22 +4,27 @@
 
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Square class, inherits from Rectangle"""
     
     def __init__(self, size, x=0, y=0, id=None):
         """initialization method"""
+        
         super().__init__(size, size, x, y, id)
     def __str__(self):
         """__str"""
+        
         return f"[square] ({self.id}) {self.x}/{self.y} - {self.width}"
     @property
     def size(self):
         """Getter size"""
+        
         return self.height
     @size.setter
     def size(self, value):
         """Setter size"""
+        
         self.width = value
         self.height = value
     def update(self, *args, **kwargs):
@@ -32,4 +37,5 @@ class Square(Rectangle):
                 setattr(self, key, value)
     def to_dictionary(self):
         """returns dictionary """
+        
         return  {'id': self.id, 'size': self.size, 'x': self.x, 'y': self.y}
