31efb84d62ee6ff8df8efda5fa6650cc79c6d775
kawtar <kawtarennabirha@gmail.com>
2024-03-01 19:11:28 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,4 @@ if __name__ == '__main__':
         print('Body response:')
         print(f'\t- type: {type(content)}')
         print(f'\t- content: {content}')
-        
+        print(f'\t- utf8 content: {content.decode("utf-8")}')
