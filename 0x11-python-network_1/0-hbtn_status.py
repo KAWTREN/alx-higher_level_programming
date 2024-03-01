e606f07e93ee62f3aab2fe204d4beb2bc33e64a8
kawtar <kawtarennabirha@gmail.com>
2024-03-01 19:06:44 +0100

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-""" module doc """
-import urllib.request
+""" Python script that fetches https://intranet.hbtn.io/status """
 
 
-if __name__ == "__main__":
-    url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        data = response.read()
-        utf_data = data.decode('utf-8')
-        resType = type(data)
-        print(f"Body response:\n\t- type: {resType}\n\t\
-- content: {data}\n\t- utf8 content: {utf_data}")
+if __name__ == '__main__':
+    from urllib import request
+
+    with request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print(f'\t- type: {type(content)}')
+        print(f'\t- content: {content}')
+        
