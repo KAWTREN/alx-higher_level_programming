1bce4f3d64c22bbdbf655d82f58e69193476d35c
kawtar <kawtarennabirha@gmail.com>
2024-04-03 22:47:52 +0000

    jQuery_Ajax

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $('#update_header').click(function() {
+      $('header').text('New Header!!!'); // Update text of header when DIV#update_header is clicked
+    });
+  });
+  
